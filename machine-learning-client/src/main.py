00a45d8237d0e11620e8e9ed4410e01a8abddf12
avaneeshdevkota <ad5768@nyu.edu>
2023-11-23 13:28:35 -0500

    Basic ML Client implementation

---
+++ machine-learning-client/src/main.py
@@ -0,0 +1,67 @@
+"""
+This module contains code for a machine learning client that creates sketch from an image.
+"""
+import os
+from datetime import datetime
+
+# pylint: disable=no-member
+import cv2
+
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+from src.defaults import TEMPLATES_DIR, INPUT_DIR, OUTPUT_DIR
+from src.model import Model
+
+
+def make_directories():
+    """
+    A function to ensure all the directories needed for the application to run are present.
+    """
+    for directory in [TEMPLATES_DIR, INPUT_DIR, OUTPUT_DIR]:
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+
+def init_app():
+    """
+    A function to initialize the application and instantiate the model used for sketching
+    """
+    make_directories()
+    app_ = Flask(__name__)
+    model_ = Model()
+
+    return app_, model_
+
+
+app, model = init_app()
+
+
+def save_image():
+    """
+    A function to save the uploaded images from the requests into a predetermined directory.
+    """
+    image = request.files["photo"]
+    timestamp = datetime.now().strftime("%Y_%m_%d_%H-%M-%S.%f")
+    image_name = f"{timestamp}_{secure_filename(image.filename)}"
+    image.save(INPUT_DIR / image_name)
+    return image_name
+
+
+@app.route("/sketch", methods=["POST"])
+def sketch():
+    """
+    A function to run the sketch model on the image.
+    """
+    image_name = save_image()
+    image = cv2.imread(str(INPUT_DIR / image_name))
+
+    try:
+        output = model.run(image)
+        cv2.imwrite(str(OUTPUT_DIR / image_name), output)
+        return {"image_name": image_name}
+    except ValueError:
+        return {"error": "No face detected in the image."}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8002)
--- machine-learning-client/src/model.py
+++ machine-learning-client/src/model.py
@@ -20,79 +20,96 @@ Asher Chan
 ```
 """
 
-from __future__ import annotations
 
+# pylint: disable=no-member
 import cv2
-import huggingface_hub
+
 import numpy as np
 import onnxruntime as ort
 import torch
 from facenet_pytorch import MTCNN
+from src.defaults import MODELS_DIR
 
 
 class Model:
+    """
+    A class to represent the model that converts images to sketches.
+    """
+
     def __init__(self):
-        self.device = torch.device(
-            'cuda:0' if torch.cuda.is_available() else 'cpu')
-        self.face_detector = MTCNN(image_size=256,
-                                   margin=80,
-                                   min_face_size=128,
-                                   thresholds=[0.7, 0.8, 0.9],
-                                   device=self.device)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.face_detector = MTCNN(
+            image_size=256,
+            margin=80,
+            min_face_size=128,
+            thresholds=[0.7, 0.8, 0.9],
+            device=self.device,
+        )
         self.model = self._load_model()
 
     @staticmethod
     def _load_model() -> ort.InferenceSession:
-        path = './models/AnimeGANv3_PortraitSketch_25.onnx'
-        breakpoint()
+        """
+        Load the model from the path.
+        """
         return ort.InferenceSession(
-            path, providers=['CPUExecutionProvider'])
+            MODELS_DIR / "AnimeGANv3_PortraitSketch_25.onnx",
+            providers=["CPUExecutionProvider"],
+        )
 
     def detect_face(self, img: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-        batch_boxes, batch_probs, batch_points = self.face_detector.detect(
-            img, landmarks=True)
+        """
+        Detect face in the image and return the bounding box and the landmarks.
+        """
+        batch_boxes, _, batch_points = self.face_detector.detect(img, landmarks=True)
         return batch_boxes, batch_points
 
     @staticmethod
-    def margin_face(box: np.ndarray,
-                    img_HW: tuple[int, int],
-                    margin: float | None = None) -> list[int]:
+    def margin_face(
+        box: np.ndarray, img_hw: tuple[int, int], margin: float | None = None
+    ) -> list[int]:
+        """
+        Get the margin of the face.
+        """
         x1, y1, x2, y2 = box
         if margin is None:
             w = x2 - x1
             h = y2 - y1
             x1 = max(0, x1 - w / 2)
             y1 = max(0, y1 - h / 2)
-            x2 = min(img_HW[1], x2 + w / 2)
-            y2 = min(img_HW[0], y2 + h / 2)
+            x2 = min(img_hw[1], x2 + w / 2)
+            y2 = min(img_hw[0], y2 + h / 2)
         else:
             x1 = max(0, x1 - margin)
             y1 = max(0, y1 - margin)
-            x2 = min(img_HW[1], x2 + margin)
-            y2 = min(img_HW[0], y2 + margin)
+            x2 = min(img_hw[1], x2 + margin)
+            y2 = min(img_hw[0], y2 + margin)
         return list(map(int, [x1, y1, x2, y2]))
 
     @staticmethod
     def resize_image(img: np.ndarray, x32: bool = True) -> np.ndarray:
+        """
+        Resize the image to a multiple of 32s.
+        """
         h, w = img.shape[:2]
         ratio = h / w
         if x32:  # resize image to multiple of 32s
-
-            def to_32s(x):
-                return 256 if x < 256 else x - x % 32
-
-            new_h = to_32s(h)
+            new_h = 256 if h < 256 else h - h % 32
             new_w = int(new_h / ratio) - int(new_h / ratio) % 32
             img = cv2.resize(img, (new_w, new_h))
         return img
 
     def preprocess_image(self, image: np.ndarray) -> np.ndarray:
+        """
+        Preprocess the image for the model.
+        """
         # BGR
-        batch_boxes, batch_points = self.detect_face(image)
+        batch_boxes, _ = self.detect_face(image)
         if batch_boxes is None:
-            print('No face detected!')
-            return
-        [x1, y1, x2, y2] = self.margin_face(batch_boxes[0], image.shape[:2])
+            return np.ndarray((0))
+        [x1, y1, x2, y2] = self.margin_face(
+            batch_boxes[0], (image.shape[0], image.shape[1])
+        )
         face_mat = image[y1:y2, x1:x2]
         face_mat = self.resize_image(face_mat)
         # BGR -> RGB
@@ -101,17 +118,23 @@ class Model:
         face_mat = np.expand_dims(face_mat, axis=0)
         return face_mat
 
-    def run_model(
-        self, face_mat: np.ndarray, img_size: tuple[int, int] = (256, 256)
-    ) -> np.ndarray:
+    def run_model(self, face_mat: np.ndarray) -> np.ndarray:
+        """
+        Run the model on the image.
+        """
         x = self.model.get_inputs()[0].name
         fake_img = self.model.run(None, {x: face_mat})
-        output_image = (np.squeeze(fake_img) + 1.) / 2 * 255
+        output_image = (np.squeeze(fake_img) + 1.0) / 2 * 255
         output_image = np.clip(output_image, 0, 255).astype(np.uint8)
         return output_image
 
     def run(self, image: np.ndarray) -> np.ndarray:
+        """
+        Run the model on the image.
+        """
         # BGR -> RGB
         data = self.preprocess_image(image[:, :, ::-1])
+        if data.size == 0:
+            raise ValueError("No face detected")
         res = self.run_model(data)
         return res  # RGB
